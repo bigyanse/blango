b5bd68a6903f1b03d04ae808319accf42607e8c7
bigyanse <contact.bigyanse@gmail.com>
2023-08-11 11:50:42 +0545

    `blango`: add initial code

---
+++ blog/tests.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from blog.models import Post
+
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
+        self.post = Post.objects.create(
+            title="The best title", body="Nice body content", author=self.user
+        )
+
+    def test_string_representation(self):
+        post = Post(title="A sample title")
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(self.post.title, "The best title")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(self.post.body, "Nice body content")
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Nice body content")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detail_view(self):
+        response = self.client.get(reverse("post_detail", args=[1]))
+        no_response = self.client.get(reverse("post_detail", args=[100000]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "The best title")
+        self.assertTemplateUsed(response, "post_detail.html")
