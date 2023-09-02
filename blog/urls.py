9e0de7f27b8eca7e5687139e7aa67e7bc8ebcb82
bigyanse <contact.bigyanse@gmail.com>
2023-09-02 20:48:20 +0545

    `feat`: added ✨ to add and view post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from blog.views import BlogListView, BlogDetailView
+from blog.views import BlogListView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
+    path("post/new", BlogCreateView.as_view(), name="post_create"),
     path("post/<int:pk>", BlogDetailView.as_view(), name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 
 from blog.models import Post
+
 
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+    fields = ["title", "author", "body"]
