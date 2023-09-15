88fa259aea4c906a2b5c3317e3f4e02c51965d6d
bigyanse <contact.bigyanse@gmail.com>
2023-09-15 16:08:12 +0545

    `style`: changed dot import to app dot import

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import RegisterView
+from accounts.views import RegisterView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register")
