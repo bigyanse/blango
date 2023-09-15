c5ddb79482d1f0f34aabb1a5e79ef0354e15087f
bigyanse <contact.bigyanse@gmail.com>
2023-09-15 17:24:20 +0545

    `refactor`: make custom user model changeable

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+from core import settings
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     body = models.TextField()
 
     def get_absolute_url(self):
