f37ab3b6b208f7cc6664dbecb10b4d13bdacf1c6
bigyanse <contact.bigyanse@gmail.com>
2023-09-17 20:09:34 +0545

    `style`: changed url just to maintain consistency in naming

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateV
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
-    path("post/new/", BlogCreateView.as_view(), name="post_create"),
+    path("post/create/", BlogCreateView.as_view(), name="post_create"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/<int:pk>/update/", BlogUpdateView.as_view(), name="post_update"),
     path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
